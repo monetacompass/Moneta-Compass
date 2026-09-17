df63d94a6a83ddc2a0904b5045d45c5511be223e
monetacompass <rng.loot2000@gmail.com>
2026-09-16 21:46:49 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@ const APP_SHELL = [
   './',
   './index.html',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
+  './Icons/icon-192.png',
+  './Icons/icon-512.png',
 ];
 
 self.addEventListener('install', (event) => {
